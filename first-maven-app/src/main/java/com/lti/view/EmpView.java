0082dbdb334bb6df5688484efda6a64f7d0fc718
SuchitraHole <prachi.hole98@gmail.com>
2021-03-25 09:48:11 +0530

    firstcommit on maven jenkins git

---
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -0,0 +1,12 @@
+ package com.lti.view;
+
+import com.lti.model.Employee;
+
+public class EmpView {
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.setEmpId(2);
+		e.setEmpName("suchitra");
+		System.out.println(e.getEmpId()+e.getEmpName());
+	}
+}
